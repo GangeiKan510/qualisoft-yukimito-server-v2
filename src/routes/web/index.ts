cc851bb08c857d5321623740d6dba9ac493e7132
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-08-18 01:26:56 +0800

    chore: removed template defaults

--- src/routes/web/index.ts
+++ src/routes/web/index.ts
@@ -1,16 +1,11 @@
 import dotenv from 'dotenv';
 import express, { Request, Response, Router } from 'express';
-import clothesRouter from './clothing';
 import userRouter from './user';
-import closetRouter from './closet';
-
 dotenv.config();
 
 const app: Router = express.Router();
 
 app.use('/users', userRouter);
-app.use('/clothes', clothesRouter);
-app.use('/closet', closetRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Web router');
