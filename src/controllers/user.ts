0e3c7134257a83326cd56b6ebc86217385365a73
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2025-01-11 01:59:09 +0800

    feat: add delete user account controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,6 @@
 import { UserProps } from '../types/user';
 import prisma from './db';
+import admin from 'firebase-admin';
 
 export const getUserByEmail = async (email: string) => {
   try {
@@ -54,5 +55,50 @@ export const updateUser = async (
   } catch (error: any) {
     console.error('Error updating user:', error);
     throw new Error(`Failed to update user: ${error.message || error}`);
+  }
+};
+
+export const deleteUser = async (userId: string) => {
+  try {
+    const userToDelete = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { pets: true, bookings: true },
+    });
+
+    if (!userToDelete) {
+      throw new Error('User not found');
+    }
+
+    const firebaseUser = await admin.auth().getUserByEmail(userToDelete.email);
+    await admin.auth().deleteUser(firebaseUser.uid);
+    console.log(`Firebase account deleted for user: ${userToDelete.email}`);
+
+    if (userToDelete.pets.length > 0) {
+      await prisma.pet.deleteMany({
+        where: { userId: userId },
+      });
+      console.log(
+        `Deleted ${userToDelete.pets.length} pets for user ${userToDelete.email}`
+      );
+    }
+
+    if (userToDelete.bookings.length > 0) {
+      await prisma.booking.deleteMany({
+        where: { user_id: userId },
+      });
+      console.log(
+        `Deleted ${userToDelete.bookings.length} bookings for user ${userToDelete.email}`
+      );
+    }
+
+    await prisma.user.delete({
+      where: { id: userId },
+    });
+    console.log(`Database entry deleted for user: ${userToDelete.email}`);
+
+    return { message: 'User deleted successfully' };
+  } catch (error: any) {
+    console.error('Error deleting user:', error);
+    throw new Error(`Failed to delete user: ${error.message || error}`);
   }
 };
